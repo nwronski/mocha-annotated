facff23abc227a6cef40d010b252570b05e59c88
Nick Wronski <nwronski@gmail.com>
2018-04-26 13:52:41 -0400

    chore(*): switch to eslint-config-nwronski-rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,5 @@
 module.exports = {
-  extends: 'airbnb-base',
-  plugins: [
-    'import',
-  ],
+  extends: 'nwronski-rules',
   env: {
     mocha: true,
     node: true,
@@ -15,42 +12,5 @@ module.exports = {
   },
   rules: {
     'no-console': 'off',
-    'import/prefer-default-export': 'off',
-    'import/no-extraneous-dependencies': 'off',
-    'array-bracket-spacing': [ 'error', 'always' ],
-    'object-curly-spacing': [ 'error', 'always' ],
-    'global-require': 'off',
-    'indent': ['error', 2, {
-      SwitchCase: 1,
-      VariableDeclarator: 1,
-      outerIIFEBody: 1,
-      MemberExpression: 0,
-      FunctionDeclaration: {
-        parameters: 1,
-        body: 1
-      },
-      FunctionExpression: {
-        parameters: 1,
-        body: 1
-      },
-      CallExpression: {
-        arguments: 1
-      },
-      ArrayExpression: 1,
-      ObjectExpression: 1,
-      ImportDeclaration: 1,
-      flatTernaryExpressions: false,
-      ignoredNodes: ['JSXElement *']
-    }],
-    'arrow-parens': [ 'error', 'always' ],
-    'comma-dangle': ['error', {
-      arrays: 'always-multiline',
-      objects: 'always-multiline',
-      imports: 'always-multiline',
-      exports: 'always-multiline',
-      functions: 'always-multiline'
-    }],
-    'max-len': [ 'error', 120 ],
-    'no-underscore-dangle': 'off',
-  }
+  },
 };
